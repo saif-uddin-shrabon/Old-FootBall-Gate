f9a0fb01504866cdf967385d6b11e3b3460a6002
saif-uddin-shrabon <saif06065@gmail.com>
2023-05-08 02:19:50 +0600

    architect add

---
+++ app/src/main/java/com/hist0r/oldfootballgate/Test.java
@@ -0,0 +1,14 @@
+package com.hist0r.oldfootballgate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
